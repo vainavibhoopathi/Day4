8ea74f91e98df06bff113b0f9c18715eed4e1e31
Vainavi <vainavibhoopathii@gmail.com>
2026-06-13 14:21:59 +0530

    Update Day8.java
    changes

--- src/Day8.java
+++ src/Day8.java
@@ -1,10 +1,27 @@
-public class Day8{
-    record ValueAddedCourse(String name,String dept){
+public class Day8 {
+    /*record ValueAddedCourse(String name,String dept){
 
     }
     static void main(){
         ValueAddedCourse v=new ValueAddedCourse("Java","I-CT");
         System.out.println(v.name());
         System.out.println(v.dept());
+    }*/
+    enum LANGUAGE {
+        EN("English"),
+        GR("German"),
+        JP("Japanese"),
+        TA("Tamil");
+        String description;
+
+        LANGUAGE(String description) {
+            this.description = description;
+        }
     }
-}
+         void main() {
+            System.out.println(LANGUAGE.EN.description);
+            System.out.println(LANGUAGE.GR.description);
+            System.out.println(LANGUAGE.JP.description);
+            System.out.println(LANGUAGE.TA.description);
+        }
+    }
